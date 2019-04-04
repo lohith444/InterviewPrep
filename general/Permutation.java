36934d8f648aebec5f84b623e08f1bc163aff697
lohith444 <lohith444@gmail.com>
2019-04-04 17:55:11 -0400

    Update Permutation.java

--- general/Permutation.java
+++ general/Permutation.java
@@ -36,11 +36,45 @@ class Solution {
     return result;
   }
   
+  private static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  
+  private static void permutation_helper(int[] arr, int start, List<int[]> result) {
+    if (start >= arr.length) {
+      result.add(arr.clone());
+    } else {
+      for (int i=start; i<arr.length; i++) {
+        swap(arr, start, i);
+        permutation_helper(arr, start+1, result);
+        swap(arr, start, i);
+      }
+    }
+  }
+  
+  public static List<int[]> permutations(int[] arr) {
+    List<int[]> result = new ArrayList<int[]>();
+    permutation_helper(arr, 0, result);
+    return result;
+  }
+  
   public static void main(String[] args) {
     String str = "abc";
     List<String> res = permutations(str);
     
     for(int i=0; i<res.size(); i++)
       System.out.println(res.get(i));
+    
+    int[] intArr = {1, 2, 3};
+    List<int[]> res2 = permutations(intArr);
+    
+    for(int i=0; i<res2.size(); i++) {
+      System.out.print("[");
+      for(int j=0; j<res2.get(i).length; j++)
+        System.out.print(res2.get(i)[j]);
+      System.out.println("]");
+    }
   }
 }
