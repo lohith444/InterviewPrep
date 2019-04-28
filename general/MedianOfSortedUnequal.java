651b1236f09490df46d5a6385c15864b31d3c541
lohith444 <lohith444@gmail.com>
2019-04-28 13:32:39 -0400

    Update MedianOfSortedUnequal.java

--- general/MedianOfSortedUnequal.java
+++ general/MedianOfSortedUnequal.java
@@ -29,49 +29,42 @@ O(log(min(m,n))) time solution.
  * If you need more classes, simply define them inline.
  */
 
-class Solution {
-  public static Double medianSorted(Integer[] arr1, Integer[] arr2) {
-    if (arr1.length == 0) {
-      return arr2.length == 0 ? null : Double.valueOf(arr2[arr2.length/2] + arr2[(arr2.length-1)/2])/2;
-    } else if (arr2.length == 0) {
-      return arr1.length == 0 ? null : Double.valueOf(arr2[arr2.length/2] + arr2[(arr2.length-1)/2])/2;
+public class Solution {
+    public static double median(int[] arr1, int[] arr2){
+        if (arr1.length > arr2.length) return median(arr2, arr1);
+        
+        int x = arr1.length;
+        int y = arr2.length;
+        
+        int low = 0;
+        int high = x;
+        
+        while(low <= high) {
+           int partitionX = (low + high)/2;
+           int partitionY = (x+y+1)/2 - partitionX;
+           
+           int leftX = (partitionX == 0) ? Integer.MIN_VALUE : arr1[partitionX - 1];
+           int rightX = (partitionX == x) ? Integer.MAX_VALUE : arr1[partitionX];
+           int leftY = (partitionY == 0) ? Integer.MIN_VALUE : arr2[partitionY - 1];
+           int rightY = (partitionY == y) ? Integer.MAX_VALUE : arr2[partitionY];
+           
+           if(leftX <= rightY && leftY <= rightX){
+               if((x+y)%2 == 0) return ((double)(Math.max(leftX, leftY) + Math.min(rightX, rightY)))/2;
+               return (double)Math.max(leftX, leftY);
+           } else if (leftX > rightY) {
+               high = partitionX - 1;
+           } else {
+               low = partitionX + 1;
+           }
+        }
+        
+        throw new RuntimeException("unreachable");
     }
-    
-    if (arr1.length > arr2.length) 
-      return medianSorted(arr2, arr1);
-    
-    int x = arr1.length;
-    int y = arr2.length;
-    int start = 0;
-    int end = x%2 == 0 ? x : x-1;
-    return median(arr1, arr2, start, end);
-  }
   
-  private static Double median(Integer[] arr1, Integer[] arr2, int start, int end) {
-    int x = arr1.length;
-    int y = arr2.length;
-    int partitionX = (start + end)/2;
-    int partitionY = (x+y+1)/2 - partitionX;
-    
-    int leftX = arr1[partitionX - 1];
-    int rightX = arr1[partitionX];
-    int leftY = arr2[partitionY - 1];
-    int rightY = arr2[partitionY];
-    
-    if(leftX <= rightY && leftY <= rightX) {
-      if ((x+y)%2 == 0) {
-        return Double.valueOf(Math.max(leftX, leftY) + Math.min(rightX, rightY))/2;
-      }
-      return Double.valueOf(Math.max(leftX, leftY));
-    } else if (leftX > rightY) {
-      return median(arr1, arr2, start, end-partitionX);
+    public static void main(String[] args) {
+        int[] arr1 = {2, 3, 7, 11};
+        int[] arr2 = {1, 5, 6, 13};
+        
+        System.out.println("Median: " + median(arr1, arr2));
     }
-    return median(arr1, arr2, start+partitionX, end);
-  }
-  
-  public static void main(String[] args) {
-    Integer[] arr1 = {2, 4, 6};
-    Integer[] arr2 = {1, 3, 5, 7};
-    System.out.println("Median of arr1 and arr2: " + medianSorted(arr1, arr2));
-  }
 }
