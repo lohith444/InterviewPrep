965d7a434907bec6edf90b033afd5faa7128f791
lohith444 <lohith444@gmail.com>
2019-05-18 20:00:20 -0400

    Update NQueens.java

--- general/NQueens.java
+++ general/NQueens.java
@@ -1,3 +1,21 @@
+/*
+ * This a trick question. The below solution is exponential time complexity.
+ * This can be solved in O(1) constant time if you see the trick of mathematical logic.
+ * Example:
+ * N = 5     
+ * [O O X O O       [O O X O
+ *  X O O O O        X O O O
+ *  O O O O X        O O O X
+ *  O X O O O        O X O O]
+ *  O O O X O]
+ * We can see that the 
+ *  - first row can be placed at x=N/2 position. Initialize i = N/2;
+ *  - second row can be placed at x-i
+ *  - third row can be placed at x+i for odd x+(i-1) for even.
+ *  - Decrement i and Repeat for rest rows: 
+ *  - fourth row can be placed at x-i. 
+ *  - fifthe row can be placed at x+i for odd x+(i-1) for even.
+ */
 import java.util.*;
 
 public class MyClass {
